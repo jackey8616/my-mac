3560bd9d018aff91985fc0fcd7c66f83390da821
Clooooode <jackey8616@gmail.com>
2024-10-25 12:04:01 +0800

    chore: update karabiner import link to repo itself

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@
 brew install --cask karabiner-elements
 
 # Install karabiner-elements vim mode config (use browser and paste this command)
-karabiner://karabiner/assets/complex_modifications/import?url=https://git.sr.ht/~harmtemolder/karabiner-vim-mode-plus/blob/master/vim_mode_plus.json
+karabiner://karabiner/assets/complex_modifications/import?url=https://raw.githubusercontent.com/jackey8616/my-mac/refs/heads/main/karabiner-import-config/vim.json
 
 # Install karabiner-elements chinese-english switch shortcuts
 # Ref: https://www.v2ex.com/t/565667
-karabiner://karabiner/assets/complex_modifications/import?url=https://gist.github.com/gxfxyz/e5f2ac1ce4f5053e9fb6608c10609837/raw/1300231ecbf9c7c87713f02c1107b9e0f8cfe553/karabiner_switch_input_source.json
+karabiner://karabiner/assets/complex_modifications/import?url=https://raw.githubusercontent.com/jackey8616/my-mac/refs/heads/main/karabiner-import-config/chinese-input.json
 
 # Amethyst: Till window manager
 brew install --cask amethyst
